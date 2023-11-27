dc8147085d62cd085ee8ec6c2d0319caa2db2e55
mikel <mikel.urrestarazu@gmail.com>
2023-11-27 16:24:27 +0100

    subida todolist

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Container from './components/Container'
+import './App.scss'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='App'>
+        <Container />
+        </div>
+    </>
+  )
+}
+
+export default App
